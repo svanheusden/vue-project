85b5814be64530f17835f17258ab8f75bdb60f99
svanheusden <velocio@gmail.com>
2022-11-27 22:03:37 +0100

    20221127-2

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,42 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+export default {
+data() {
+  return {
+    titleClass: 'title',
+    message: 'Welcome to Your Vue.js App',
+    count: 0,
+    awesome: true,
+    text:''
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    toggle() {
+      this.awesome = !this.awesome
+    }
+      
+    }
+  }
+
+
 </script>
 
+
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <button @click="toggle">toggle</button>
+<h1 v-if="awesome" :class='titleClass'>{{ message }}</h1>
+<h1 v-else>Oh no</h1>
+<input v-model="text" placeholder="Type here"/>
+<p>{{ text }}</p>
+<button @click="increment">{{ count }}</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+.title {
+  font-size: 40px;
+  font-weight: 200;
+  color: red;
 }
 </style>
