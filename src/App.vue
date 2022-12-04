1a97fc3ba166b7a19726225ab840bc8a99d153a6
svanheusden <velocio@gmail.com>
2022-12-04 14:16:49 +0100

    20221204-1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script etup lang="ts">
+let id=0
+
 export default {
 data() {
   return {
+    newTodo: '',
+    todos: [
+      {
+        id: id++,
+        text: 'Learn Vue 3',
+        
+      },
+      {
+        id: id++,
+        text: 'Learn Vue Router',
+        
+      },
+    ],
     titleClass: 'title',
     message: 'Welcome to Your Vue.js App',
     count: 0,
@@ -15,11 +30,20 @@ data() {
     },
     toggle() {
       this.awesome = !this.awesome
-    }
-      
-    }
-  }
-
+    },
+    addTodo() {
+      this.todos.push({
+        id: id++,
+        text: this.newTodo    
+      })
+      this.newTodo = ''
+        },
+      removeTodo(todo) {
+        this.todos = this.todos.filter((t) => t !== todo)
+      }
+       }      
+      }
+    
 
 </script>
 
@@ -30,7 +54,25 @@ data() {
 <h1 v-else>Oh no</h1>
 <input v-model="text" placeholder="Type here"/>
 <p>{{ text }}</p>
-<button @click="increment">{{ count }}</button>
+
+
+<form @submit.prevent="addTodo">
+  <input v-model="newTodo" placeholder="Add a new todo">
+  <button type="submit">Add</button>
+</form> 
+
+<ul>
+  <li v-for="todo in todos" :key="todo.id">
+    {{ todo.text }}
+    <button @click="removeTodo(todo)">X</button>
+  </li>
+
+
+
+</ul>
+
+
+
 </template>
 
 <style>
